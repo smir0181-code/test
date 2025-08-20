3370e7a59e59cc703a2b907a2041a47759081b46
smir0181-code <smir0181@gmail.com>
2025-08-20 08:26:36 +0500

    Добавлена функция установки времени

--- Remider.py
+++ Remider.py
@@ -5,12 +5,30 @@ import datetime
 import time
 import pygame
 
+
+def set():
+    rem=sd.askstring("Время напоминания","Введите время напоминания в формате ЧЧ:ММ(в 24 часовом формате)")
+    if rem:
+        try:
+            hour=int(rem.split(":")[0])
+            minute=int(rem.split(":")[1])
+            now=datetime.datetime.now()
+            print(now)
+            dt=now.replace(hour=hour, minute=minute)
+            print(dt)
+            t=dt.timestamp
+            print(t)
+        except Exception as e:
+            mb.showerror("Ошибка",f"Некорректное время{e}")
+         
+
+
 window = Tk()
 window.title("Напоминание")
 window.geometry("300x250")
-label= Label(text="Установите напоминание")
+label= Label(text="Установите напоминание",font=23)
 label.pack(pady=10)
-set_button = Button(text="Установить", command=set_reminder)
+set_button = Button(text="Установить", command=set)
 set_button.pack(pady=10)
 
 
